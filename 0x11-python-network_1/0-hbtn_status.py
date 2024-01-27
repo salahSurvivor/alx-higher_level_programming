a808e5d9efc89d0997e1deec27209e8aa415f178
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-01-27 13:05:10 -0800

    commit task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib
+
+This script sends a GET request to the specified URL and prints
+information about the response body, its type, content, and utf-8 content.
+"""
+
 import urllib.request
 
+def fetch_status(url):
+
+    with urllib.request.urlopen(url) as response:
+        return response.read()
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        r = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    body = fetch_status(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
