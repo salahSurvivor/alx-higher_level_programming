3c525bd543d5a882a9654c798004a5278faf1fba
salahSurvivor <salaheddinenaji2000@gmail.com>
2024-01-27 13:14:32 -0800

    fix task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""
-Fetches https://alx-intranet.hbtn.io/status using urllib
-
-This script sends a GET request to the specified URL and prints
-information about the response body, its type, content, and utf-8 content.
-"""
+"""This script gets the status of https://alx-intranet.hbtn.io using urllib"""
 
 import urllib.request
 
-def fetch_status(url):
 
+def fetch_status():
+    """GETS the status of the URL & SHOWS the response body"""
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        return response.read()
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    
-    body = fetch_status(url)
 
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __name__ == '__main__':
+    fetch_status()
